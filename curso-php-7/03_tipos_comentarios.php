90fa76229a9b2fc48ac2ba21ed01c7e7f1d5de3d
nely de faria <nelly.nf54@gmail.com>
2021-04-09 20:31:32 -0300

    Adiciona arquivo 03_tipos_comentarios.php

---
+++ curso-php-7/03_tipos_comentarios.php
@@ -0,0 +1,15 @@
+<?php
+
+$txt = "PHP";
+
+echo "I love $txt!";
+
+// This is a single-line comment
+
+# This is a single-line comment
+
+/* 
+This is a multiple-lines comments block
+that spans over multiple
+lines
+*/
