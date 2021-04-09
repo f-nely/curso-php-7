e427df1ee9cb01dbf953bc431de0a263cb6bc87b
nely de faria <nelly.nf54@gmail.com>
2021-04-09 20:33:42 -0300

    Adiciona arquivo 06_arrays.php

---
+++ curso-php-7/06_arrays.php
@@ -0,0 +1,44 @@
+<?php
+
+/* $cars = ['S10', 'Jeep Compass', 'Renegade', 'Jetta'];
+
+var_dump($cars); */
+
+/* $car = [
+    'cor' => 'Branco',
+    'portas' => 4,
+    'ano' => 2018
+];
+$car['airbag'] = true;
+
+//echo $car['cor'];
+//var_dump($car);
+
+foreach($car as $c) {
+    echo $c . '<br>';
+} */
+
+$cars = [
+    'Mercedes' => [
+        'color' => 'black',
+        'motor' => 5.2,
+        'year' => 2017
+    ],
+    'Ford' => [
+        'color' => 'blue',
+        'motor' => 3.2,
+        'year' => 2019
+    ],
+    'Ferrari' => [
+        'color' => 'red',
+        'motor' => 5.2,
+        'year' => 2020
+    ]
+];
+
+//echo $cars['Ford']['color'];
+
+foreach($cars as $car) {
+    echo 'Color: ' . $car['color']  . ' '. 'Motor: '. $car['motor'] .' '. 'Year: '. $car['year'] . '<br>';
+}
+
