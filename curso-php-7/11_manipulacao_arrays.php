46b4696304da1d5dbcb34834fee0fa74fe370db8
nely de faria <nelly.nf54@gmail.com>
2021-04-14 22:09:55 -0300

    Usando array_filter

--- curso-php-7/11_manipulacao_arrays.php
+++ curso-php-7/11_manipulacao_arrays.php
@@ -2,5 +2,10 @@
 
 $ages = [12, 14, 18, 20, 44, 50, 98, 78, 56];
 
-echo end($ages); // retorna o último elemento do array
+//echo end($ages); // retorna o último elemento do array
 
+$agesFiltered = array_filter($ages, function ($age) {
+    return $age >= 18;
+});
+
+var_dump($agesFiltered);
