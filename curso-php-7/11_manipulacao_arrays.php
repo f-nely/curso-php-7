b390e23669e527c3500de1d3545e86317d3dc590
nely de faria <nelly.nf54@gmail.com>
2021-04-14 22:22:07 -0300

    Usando função array_map

--- curso-php-7/11_manipulacao_arrays.php
+++ curso-php-7/11_manipulacao_arrays.php
@@ -8,4 +8,17 @@ $agesFiltered = array_filter($ages, function ($age) {
     return $age >= 18;
 });
 
-var_dump($agesFiltered);
+//var_dump($agesFiltered);
+
+$names = ['Carlos', 'EspecializaTi', 'Company'];
+/* $names[0] = strtoupper($names[0]);
+echo $names[0]; */
+
+$names = array_map('applyToupper', $names);
+
+function applyToupper($value)
+{
+    return strtoupper($value);
+}
+
+var_dump($names);
