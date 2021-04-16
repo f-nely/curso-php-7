5ea8adeb87ba5a6142027876867549abc349c95d
nely de faria <nelly.nf54@gmail.com>
2021-04-16 11:02:18 -0300

    Adiciona arquivo index.php

---
+++ curso-php-7/site/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>Sou o header da página</h1>
+    </header>
+    
+    <div>
+        <?php
+            echo 'Olá mundo!';
+        ?>
+    </div>
+
+    <footer>
+        <p>Rodapé</p>
+    </footer>
+</body>
+</html>
