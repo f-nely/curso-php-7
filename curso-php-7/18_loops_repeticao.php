bce5ec9e2b8df8d9a64aa5663767cf627945395a
nely de faria <nelly.nf54@gmail.com>
2021-04-19 20:58:33 -0300

    Adiciona arquivo 18_loops_repeticao.php

---
+++ curso-php-7/18_loops_repeticao.php
@@ -0,0 +1,25 @@
+<?php
+
+$cars = ['Volvo', 'BMW', 'Toyota', 'S10'];
+
+for ($i = 0; $i < count($cars); $i++) {
+    echo "{$cars[$i]} <br>";
+}
+
+$fruits = ['Apple', 'Banana', 'Orange'];
+
+for ($i = 0; $i < count($fruits); $i++) {
+    echo "{$fruits[$i]} ";
+}
+
+/* foreach ($fruits as $fruit) {
+    echo "{$fruit} <br>";
+} */
+
+/* echo '<hr>';
+
+for ($i = 0; $i <= 10; $i++) {
+    echo "{$i} <br>";
+}
+
+echo '<hr>'; */
