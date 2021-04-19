cab1921b0e653fab222d104a4e4edbb03701944a
nely de faria <nelly.nf54@gmail.com>
2021-04-18 22:20:31 -0300

    Adiciona arquivo 17_switch.php

---
+++ curso-php-7/17_switch.php
@@ -0,0 +1,18 @@
+<?php
+
+$number = 14;
+
+switch ($number) {
+    case 12:
+        echo 'Doze';
+        break;
+    case 13:
+        echo 'Treze';
+        break;
+    case 14:
+        echo 'Quatorze';
+        break;
+    default:
+        echo 'Não identificado';
+        break;
+}
