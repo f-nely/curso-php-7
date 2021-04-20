4ffbdc6dffc287095dcd1d7a182b7fc57b404675
nely de faria <nelly.nf54@gmail.com>
2021-04-19 21:12:30 -0300

    Adiciona arquivo 19_loops_repeticao.php

---
+++ curso-php-7/19_loops_repeticao.php
@@ -0,0 +1,19 @@
+<?php
+
+$ages = [
+    'Peter' => 35,
+    'Ben'   => 37,
+    'Joe'   => 43,
+];
+
+foreach ($ages as $key => $age) {
+    echo "Key = {$key}, Value =  {$age} <br>";
+}
+
+
+//$names = ['Peter', 'Ben', 'Joe'];
+/* foreach ($names as $key => $name) {
+    echo "{$key} => {$name} <br>";
+} */
+
+
