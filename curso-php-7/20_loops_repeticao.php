fa325644615a666972af2b0591b7e1be8af7988c
nely de faria <nelly.nf54@gmail.com>
2021-04-21 18:20:43 -0300

    Adiciona arquivo 20_loops_repeticao.php

---
+++ curso-php-7/20_loops_repeticao.php
@@ -0,0 +1,17 @@
+<?php
+
+$cars = ['S10', 'Hb20', 'Jetta', 'Jeep Compass', 'Renegade'];
+
+$j = 0;
+
+while ($j < count($cars)) {
+    echo "{$cars[$j]} <br>";
+    $j++;
+}
+
+$i = 0;
+do {
+    echo "{$i} <br>";
+    $i++;
+
+} while($i < 10);
