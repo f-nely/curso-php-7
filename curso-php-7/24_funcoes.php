c131b8f87aef0cf15e0bcd274e63d9f7245d7012
nely de faria <nelly.nf54@gmail.com>
2021-04-22 19:22:39 -0300

    Adiciona arquivo 24_funcoes.php

---
+++ curso-php-7/24_funcoes.php
@@ -0,0 +1,13 @@
+<?php
+
+function fatorial(int $number): int
+{
+    if ($number <= 1) 
+        $number = $number;
+    else 
+        $number *= fatorial($number - 1);
+
+    return $number;
+}
+
+echo fatorial(4);
