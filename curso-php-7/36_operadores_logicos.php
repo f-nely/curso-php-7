e48a1e40221ff5adf5a77db1be6eeeb72eb9a3cd
nely de faria <nelly.nf54@gmail.com>
2021-04-28 16:47:48 -0300

    Adiciona arquivo 36_operadores_logicos.php

---
+++ curso-php-7/36_operadores_logicos.php
@@ -0,0 +1,40 @@
+<?php
+
+$a = 2;
+$b = 2;
+
+/* if ($a == $b AND $a == 2) {
+    echo 'No IF';
+} else {
+    echo 'No ELSE';
+} */
+
+/* if ($a != $b OR $a == 3) {
+    echo 'No IF';
+} else {
+    echo 'No ELSE';
+} */
+
+/* if ($a == $b && $a == 2) {
+    echo 'No IF';
+} else {
+    echo 'No ELSE';
+} */
+
+/* if ($a == $b || $a == 3) {
+    echo 'No IF';
+} else {
+    echo 'No ELSE';
+} */
+
+/* if (($a == $b AND $a == 2) || $b == 3) {
+    echo 'No IF';
+} else {
+    echo 'No ELSE';
+} */
+
+if (!$a == !2) {
+    echo 'No IF';
+} else {
+    echo 'No ELSE';
+}
