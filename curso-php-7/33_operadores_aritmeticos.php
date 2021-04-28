03d1f7b9e0309cccb1d52a3f7a2e1d337135428c
nely de faria <nelly.nf54@gmail.com>
2021-04-28 16:45:26 -0300

    Adiciona arquivo 33_operadores_aritmeticos.php

---
+++ curso-php-7/33_operadores_aritmeticos.php
@@ -0,0 +1,21 @@
+<?php
+
+$a = 2;
+$b = 4;
+
+$result = $a + $b;
+echo "O resultado da soma entre {$a} + {$b} = {$result} <br>";
+
+$result = $b - $a;
+echo "O resultado da subtração entre {$b} - {$a} = {$result} <br>";
+
+$result = $b * $a;
+echo "O resultado da multiplicação entre {$b} * {$a} = {$result} <br>";
+
+$result = $b / $a;
+echo "O resultado da divisão entre {$b} / {$a} = {$result} <br>";
+
+$result = $b % $a;
+echo "O resultado do resto da divisão entre {$b} % {$a} = {$result} <br>";
+
+/* Believe and act as if it were impossible to fail*/
