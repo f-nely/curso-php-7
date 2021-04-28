7614b053c76977331377f3b565baaba2ee8430d2
nely de faria <nelly.nf54@gmail.com>
2021-04-28 16:48:26 -0300

    Adiciona arquivo 37_operadores_relacionais.php

---
+++ curso-php-7/37_operadores_relacionais.php
@@ -0,0 +1,28 @@
+<?php
+
+$a = 2;
+$b = 3;
+
+/* if ($a === $b) {
+    echo 'IF';
+} else {
+    echo 'ELSE';
+} */
+
+/* if ($a !== $b) {
+    echo 'IF';
+} else {
+    echo 'ELSE';
+} */
+
+/* if ($a >= $b) {
+    echo 'IF';
+} else {
+    echo 'ELSE';
+} */
+
+if ($a <= $b) {
+    echo 'IF';
+} else {
+    echo 'ELSE';
+}
