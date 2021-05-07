51fe4ab8038c2911801916773511edfc5eda1467
nely de faria <nelly.nf54@gmail.com>
2021-05-07 20:45:21 -0300

    Adiciona arquivo files.php

---
+++ curso-php-7/files/files.php
@@ -0,0 +1,8 @@
+<?php
+
+//var_dump(touch('test.txt')); 
+//var_dump(rename('test.txt', 'test2.txt'));
+//var_dump(file_exists('test2.txt'));
+//var_dump(copy('test2.txt', 'test.txt'));
+//var_dump(unlink('test2.txt'));
+var_dump(unlink('test.txt'));
