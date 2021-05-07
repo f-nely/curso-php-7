0902c1729fff1353b2afc024766048e9ab195517
nely de faria <nelly.nf54@gmail.com>
2021-05-07 20:44:51 -0300

    Adiciona arquivo directory.php

---
+++ curso-php-7/directories/directory.php
@@ -0,0 +1,14 @@
+<?php
+
+//var_dump(is_dir('teste'));
+//var_dump(is_dir('test'));
+//var_dump(mkdir('uploads'));
+//var_dump(mkdir('uploads/teste'));
+//var_dump(rmdir('uploads/teste'));
+//var_dump(rmdir('uploads'));
+//echo getcwd();
+//var_dump(scandir('..'));
+//var_dump(glob('../*'));
+var_dump(glob('../*.php'));
+
+/* if there is no struggle, there is no progress */
