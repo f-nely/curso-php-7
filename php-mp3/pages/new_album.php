85bb2a6e76c8f4651ec36791ada33d843b9fb601
nely de faria <nelly.nf54@gmail.com>
2021-05-12 20:32:07 -0300

    Adiciona arquivo new_album.php

---
+++ php-mp3/pages/new_album.php
@@ -0,0 +1,15 @@
+<div class="form-group">
+    <a href="?page=albums" class="btn btn-primary mt-2 ml-2">Voltar Para os Álbuns</a>
+</div>
+
+<h1>Cadastrar Novo Álbum</h1>
+
+<form action="#" method="post">
+    <div class="form-group">
+        <input type="text" name="name" placeholder="Nome:" class="form-control">
+    </div>
+    <div class="form-group">
+        <input type="file" name="image" accept="image/*" class="form-control">
+    </div>
+    <button type="submit" class="btn btn-success ml-2">Cadastrar</button>
+</form>
