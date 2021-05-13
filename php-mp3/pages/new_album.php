90edcf6dfa536317e0840c5ae19a2dc1fc413840
nely de faria <nelly.nf54@gmail.com>
2021-05-13 20:58:37 -0300

    Fazendo upload novo álbum

--- php-mp3/pages/new_album.php
+++ php-mp3/pages/new_album.php
@@ -4,7 +4,7 @@
 
 <h1>Cadastrar Novo Álbum</h1>
 
-<form action="#" method="post">
+<form action="#" method="POST" enctype="multipart/form-data">
     <div class="form-group">
         <input type="text" name="name" placeholder="Nome:" class="form-control">
     </div>
@@ -13,3 +13,28 @@
     </div>
     <button type="submit" class="btn btn-success ml-2">Cadastrar</button>
 </form>
+
+<?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        //var_dump($_FILES['image']);
+        $album = $_POST['name'];
+
+        $path = "albums/{$album}";
+
+        if (!is_dir($path)) {
+            mkdir($path);
+        }
+
+        $file = $_FILES['image'];
+        $fileInfo = explode('.', $file['name']);
+
+        $extension = $fileInfo[1];
+        $nameImage = $album . '.' . $extension;
+
+        if (move_uploaded_file($file['tmp_name'], $path.'/'.$nameImage)) {
+            header('Location: ?page=albums');
+        } else {
+            echo 'Falha no upload...';
+        }
+    }
+?>
